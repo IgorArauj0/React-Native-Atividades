aed836bec19a8808fee84aed95996343b7b01889
IgorArauj0 <igoritoaraujo223@gmail.com>
2026-03-09 19:00:23 -0300

    save

--- app-aulas/App.js
+++ app-aulas/App.js
@@ -1,29 +1,42 @@
 //Arquivo Principal onde fica o componente raiz
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 //Aqui são os import, ou seja, importamos aquilo que queremos utilizar
+import { useState } from 'react';
 
+
+//contador = guarda o valor 
+// setContador = atualiza o valor
+// 0 = valor inicial
 export default function App() {
+
+  const [contador, setContador] = useState(0);
+
+
   return (
     <View style={styles.container}>
-    
+
 
       <View style={styles.primeiraView}>
-         <Text>Conteúdo da Primeira View</Text>
-         <Text>Conteúdo da Primeira View</Text>
+        <Text>Contador:</Text>
+        <Text>{contador}</Text>
+
+        <Button title="Aumentar contador" onPress={() => setContador(contador + 1)}
+
+        />
       </View>
 
       <View style={styles.segundaView}>
-         <Text>Conteúdo da Segunda View</Text>
-         <Text>Conteúdo da Segunda View</Text>
+        <Text>Testando</Text>
       </View>
-
-
 
       <StatusBar style="auto" />
     </View>
-    
+
   );
+
+
+
 }
 
 const styles = StyleSheet.create({
@@ -41,7 +54,7 @@ const styles = StyleSheet.create({
     padding: 30,
   },
 
-   segundaView: {
+  segundaView: {
     backgroundColor: 'blue',
     justifyContent: 'center',
     alignItems: 'center',
